fcab9b2303b4e86e3668eb6fd833b588422b7a84
laurivasyyy <jalusakti56@gmail.com>
2023-12-28 23:02:49 +0700

    initial commit

---
+++ app/Views/page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Hello, World!</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #666;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a simple HTML page displaying a 'Hello, World!' message.</p>
+</body>
+
+</html>
